14dcb4376c5531b98cc963a55fa0193821e048e4
tusharswami534 <tusharswami17@gmail.com>
2024-11-30 21:36:30 +0530

    home js change

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,13 +1,12 @@
 // Ensure GSAP and ScrollTrigger are registered
 gsap.registerPlugin(ScrollTrigger);
 
-// Animate the progress line based on scroll
 gsap.to("#scroll-line", {
-  width: "100%", // Full width of the viewport
+  width: "100%", 
   scrollTrigger: {
-    trigger: "#section-to-pin", // The section being tracked
-    start: "top top", // Start tracking when the section reaches the top of the viewport
-    end: "bottom top", // End tracking when the bottom of the section reaches the top of the viewport
-    scrub: true, // Smooth animation during scroll
+    trigger: "#section-to-pin",
+    start: "top top", 
+    end: "bottom top", 
+    scrub: true, 
   },
 });
